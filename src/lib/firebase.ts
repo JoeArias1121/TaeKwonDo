dcddf4fe41980fb931008cdc929d1051729d05f9
joearias1121 <joearias1121@gmail.com>
2026-04-27 23:35:17 -0400

    fix: hardcode public firebase config for CI/CD

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyB1j3Q5Jm0qRMOk4d6EjbJauB69Z6krXQE",
+  authDomain: "tkdweb-38f7b.firebaseapp.com",
+  projectId: "tkdweb-38f7b",
+  storageBucket: "tkdweb-38f7b.firebasestorage.app",
+  messagingSenderId: "356882081213",
+  appId: "1:356882081213:web:df9bd8c9ff18e610d45ed4"
 };
 
 // Initialize Firebase
